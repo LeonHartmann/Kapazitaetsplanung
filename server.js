9fc67bc20c8574a7e2ff2a10b521c75afa95fa1f
Leon Hartmann <Leon.hartmann@sportfive.com>
2025-04-27 18:05:05 +0200

    Update package.json to include Express server, modify start script, and add project description and license

---
+++ server.js
@@ -0,0 +1,8 @@
+// server.js
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => console.log(`🚀 Listening on ${port}`));
